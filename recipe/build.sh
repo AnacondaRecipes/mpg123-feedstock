66d5582a82022aa7dc36391c5772a76d7cecc13a
Ivan Iziumov <iiziumov@anaconda.com>
2026-01-28 13:35:40 +0100

    mpg123 1.33.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,6 @@
 # Get an updated config.sub and config.guess
 cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build
 
-./configure prefix=$PREFIX
+./configure --prefix=${PREFIX}
 make
 make install
